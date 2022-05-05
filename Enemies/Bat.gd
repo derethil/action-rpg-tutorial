bb43f6b9e917512f8a0d3800f64c6da2c40124c9
Jaren Glenn <jarenglenn@gmail.com>
2022-05-05 14:12:31 -0600

    added stats to bat and sword

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,13 +1,21 @@
 extends KinematicBody2D
 
-const KNOCKBACK_FRICTION = 200
-const KNOCKBACK_SPEED = 120
+export var KNOCKBACK_FRICTION = 200
+export var KNOCKBACK_SPEED = 120
 
 var knockback = Vector2.ZERO
+
+onready var stats = $Stats
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, KNOCKBACK_FRICTION * delta)
 	knockback = move_and_slide(knockback)
+
 	
 func _on_Hurtbox_area_entered(area):
+	stats.health -= area.damage
 	knockback = area.knockback_vector * KNOCKBACK_SPEED
+
+
+func _on_Stats_no_health():
+	queue_free()
